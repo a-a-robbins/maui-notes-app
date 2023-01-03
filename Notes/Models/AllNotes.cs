4774518752cfca88816e2039cc9c25f6b5306a23
Ashley Robbins <robbinsa1@central.edu>
2023-01-02 19:26:45 -0600

    initial upload

---
+++ Notes/Models/AllNotes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel; 
+namespace Notes.Models;
+
+internal class AllNotes
+{
+    public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>(); 
+
+    public AllNotes() 
+    {
+        LoadNotes(); 
+    }
+
+    public void LoadNotes()
+    {
+        Notes.Clear();
+
+        //get folder where notes stored
+        string appDataPath = FileSystem.AppDataDirectory;
+
+        //use linq to load *.notes.txt file
+        IEnumerable<Note> notes = Directory
+
+            //select the file names from directory
+            .EnumerateFiles(appDataPath, "*.notes.txt")
+
+            //use file name to create new note
+            .Select(filename => new Note()
+            {
+                Filename = filename,
+                Text = File.ReadAllText(filename),
+                Date = File.GetCreationTime(filename)
+            })
+
+            //order notes by date
+            .OrderBy(note => note.Date);
+
+        //add notes to the collection
+        foreach (Note note in notes)
+        {
+            Notes.Add(note); 
+        }
+    }
+}
+
---
+++ Notes/Views/AllNotes.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace Notes.Views;
+
+public partial class AllNotes : ContentPage
+{
+	public AllNotes()
+	{
+		InitializeComponent();
+
+		BindingContext = new Models.AllNotes(); 
+	}
+
+    protected override void OnAppearing()
+    {
+        ((Models.AllNotes)BindingContext).LoadNotes(); 
+    }
+
+    private async void Add_Clicked (object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(NotePage)); 
+    }
+
+    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if(e.CurrentSelection.Count != 0)
+        {
+            //get the note model
+            var note = (Models.Note)e.CurrentSelection[0];
+            
+            //go to note page
+            await Shell.Current.GoToAsync($"{nameof(NotePage)} ? {nameof(NotePage.ItemId)} = {note.Filename}");
+
+            //unselect the ui
+            notesCollection.SelectedItem = null; 
+        }
+
+    }
+}
